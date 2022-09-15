cdfd1de5af2a4560a9585733b5fb3cb7c946c005
unknown <becomingguru@gmail.com>
2022-09-14 19:03:46 -0600

    clenaup

--- background.js
+++ background.js
@@ -1,27 +1,4 @@
-let color = '#3aa757';
-
-function hideStuff(event) {
-    event.button.textContent = "Clicked";
-}
-
 chrome.runtime.onInstalled.addListener(() => {
     //   chrome.storage.sync.set({ color });
-    console.log("onInstalled triggered");
+    console.log("chromeBlocker installed");
 });
-
-console.log("background.js parsed");
-
-chrome.action.onClicked.addListener((tab) => {
-
-    notehuneth();
-
-    let button = chrome.action.getPopup().document.getElementById("hideStuff");
-    button.textContent = "Click from popup.js listener";
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['popup.js']
-    });
-});
-
-console.log("background.js: supposedly processed action.onclicked listener");
--- popup.js
+++ popup.js
@@ -1,46 +1,15 @@
-// alert('Hello world');
-
-function setBackgroundColor() {
-    console.log("body script");
-    document.body.style.backgroundColor = blue;
-}
-
 function hideAmazonStuff() {
     
-    let backgroundPage = chrome.extension.getBackgroundPage();
-    // backgroundPage.console.log('foo');
-
     chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
         chrome.scripting.executeScript({
             target: {tabId: tab.id},
             func: () => {
-                document.body.style.backgroundColor = "blue";
+                document.getElementById("nav-main").setAttribute("style","display: none;");
             }
         });
     });
-
-
-
-    // chrome.scripting.executeScript({
-    //     target: { tabId: tab.id },
-    //     func: setPageBackgroundColor,
-    // });
-}
-
-function getButton() {
-    return document.getElementById("hideStuff");
-}
-
-let backgroundPage = chrome.extension.getBackgroundPage();
-
-function buttonListener(event) {
-    getButton().textContent = "buttonListener";
-    hideAmazonStuff();
 }
 
 window.addEventListener("load", (event) => {
-    let button = document.getElementById("hideStuff");
-    button.textContent = "onLoad2";
-    button.addEventListener("click", buttonListener);
-    console.log("onLoad");
+    button.addEventListener("click", (event) => hideAmazonStuff());
 });
